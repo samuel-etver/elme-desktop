e1e269f39c3672059d309324c41dbea79f66f00d
samuel-etver <samuel.etver@gmail.com>
2020-06-15 23:27:40 +0500

    started working on archive page

--- src/renderer/components/ArchivePage.jsx
+++ src/renderer/components/ArchivePage.jsx
@@ -1,14 +1,61 @@
 import React from 'react';
 import './ArchivePage.css';
+import Constants from '../../common/Constants';
+import MeasureParameters from '../MeasureParameters';
+import EventManager from '../../common/EventManager';
+import MainEventManager from '../../common/MainEventManager';
+import MeasureParametersComboBox from './MeasureParametersComboBox';
+import NumberButtonsGroup from './NumberButtonsGroup';
+import HorzDivider from './HorzDivider';
+import ChartBuilder from '../ChartBuilder';
+import Chart from './Chart';
+
+let mainEventManager = MainEventManager.getInstance();
 
 class ArchivePage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.prefix = 'archive-page-';
+        this.eventManager = new EventManager();
+        this.measureParameters = new MeasureParameters();
+        let measureParameter = this.measureParameters.get('inductorTemperature1');
+        this.state = {
+            selectedMeasureParameterId: measureParameter.id,
+        };
+    }
+
+
+    componentDidMount() {
+
+    }
+
+
+    componentWillUnmount() {
+
+    }
+
+
     render() {
         let style = 'archive-page ';
         if ( this.props.style ) {
             style += this.props.style;
         }
-        return <div class={style}>
-               </div>;
+        let chartCaptionOptions = {
+            prefix: this.prefix,
+            selectedId: this.state.selectedMeasureParameterId,
+            eventManager: this.eventManager
+        };
+        let chartNumberButtonsGroupOptions = {
+            prefix: this.prefix,
+            count: this.measureParameters.size(),
+            eventManager: this.eventManager
+        };
+
+        return  <div class={style}>
+                    <MeasureParametersComboBox options={chartCaptionOptions}/>
+                    <HorzDivider height="80px" />
+                    <NumberButtonsGroup options={chartNumberButtonsGroupOptions}/>
+                </div>;
     }
 }
 
---
+++ src/renderer/components/EditableComboBox.jsx
@@ -0,0 +1,12 @@
+import './EditableComboBox.css';
+import React from 'react';
+
+
+class EditableComboBox extends React.Component {
+    render() {
+        return  <div>
+                </div>
+    }
+}
+
+export default EditableComboBox;
