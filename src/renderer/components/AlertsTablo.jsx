3591466d56c3ef13269394ab645637aaefba4e53
Samuel Etver <61624124+samuel-etver@users.noreply.github.com>
2020-09-18 15:36:33 +0500

    working on alerts tablo

---
+++ src/renderer/components/AlertsTablo.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import './AlertsTablo.css';
+import AlertStorage from '../AlertsStorage';
+import MainEventManager from '../../common/MainEventManager';
+
+let alertsStorage = AlertStorage.getInstance();
+let mainEventManager = MainEventManager.getInstance();
+
+class AlertsTablo extends React.Component {
+    constructor() {
+        super();
+        this.onAppLoad = this.onAppLoad.bind(this);
+        this.onAppClose = this.onAppClose.bind(this);
+        mainEventManager.subscribe('app-load', this.onAppLoad);
+    }
+
+
+    componentDidMount() {
+
+    }
+
+
+    componentWillUnmount() {
+
+    }
+
+
+onAppLoad() {
+    mainEventManager.unsubscribe('app-load', this.onAppLoad);
+    mainEventManager.subscribe('app-close', this.onAppClose);
+}
+
+
+onAppClose() {
+    mainEventManager.unsubscribe('app-close', this.onAppClose);
+}
+
+
+    render() {
+        return  <div class="alerts-tablo">
+                </div>
+    }
+}
+
+export default AlertsTablo;
--- src/renderer/components/NotificationPane.jsx
+++ src/renderer/components/NotificationPane.jsx
@@ -2,37 +2,14 @@
 import React from 'react';
 import './NotificationPane.css'
 import Clock from './Clock';
-import AlertStorage from '../AlertsStorage';
-import MainEventManager from '../../common/MainEventManager';
-
-let alertsStorage = AlertStorage.getInstance();
-let mainEventManager = MainEventManager.getInstance();
+import AlertsTablo from './AlertsTablo';
 
 class NotificationPane extends React.Component {
-    constructor(props) {
-        super(props);
-        this.timerId = undefined;
-        this.onAppLoad = this.onAppLoad.bind(this);
-        this.onAppClose = this.onAppClose.bind(this);
-        mainEventManager.subscribe('app-load', this.onAppLoad);
-    }
-
-
-    onAppLoad() {
-        mainEventManager.unsubscribe('app-load', this.onAppLoad);
-        mainEventManager.subscribe('app-close', this.onAppClose);
-    }
-
-
-    onAppClose() {
-        mainEventManager.unsubscribe('app-close', this.onAppClose);
-    }
-
-
     render() {
         return <div class="notification-pane">
                   <Clock />
                   <div class="notiication-horizontal-divider" />
+                  <AlertsTablo />
                </div>
     }
 }
