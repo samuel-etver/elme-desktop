102c1d209e5fbe3f391f9635feb5098ccbc84dc3
Samuel Etver <61624124+samuel-etver@users.noreply.github.com>
2020-09-22 15:25:01 +0500

    working on alert tablo

--- src/renderer/components/AlertsDropdown.jsx
+++ src/renderer/components/AlertsDropdown.jsx
@@ -5,13 +5,29 @@ import './AlertsDropdown.css';
 class AlertsDropdown extends React.Component {
     constructor() {
         super();
+        this.onCloseClick = this.onCloseClick.bind(this);
     }
 
-    
+
+    onCloseClick() {
+
+    }
+
+
     render() {
         return  <div class="alerts-dropdown">
                     <div class="alerts-dropdown-button" />
                     <div class="alerts-dropdown-image" />
+                    <div class="alerts-dropdown-container">
+                        <div class="alerts-dropdown-header">
+                            <div class="alerts-dropdown-column-date">Дата</div>
+                            <div class="alerts-dropdown-column-time">Время</div>
+                            <div class="alerts-dropdown-column-alert">
+                                <div class="alerts-dropdown-column-alert-text">Сообщения</div>
+                                <div class="alerts-dropdown-close"></div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
     }
 }
