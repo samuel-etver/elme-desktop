59cd0b94eb9afc4818100ce3b2a6afae58b33194
Samuel Etver <61624124+samuel-etver@users.noreply.github.com>
2021-12-13 10:45:19 +0500

    code review

--- src/renderer/components/AlertsTablo.jsx
+++ src/renderer/components/AlertsTablo.jsx
@@ -12,82 +12,52 @@ class AlertsTablo extends React.Component {
         this.currAlerts = [];
         this.shutterRef = React.createRef();
         this.alertIndex = 0;
-        this.timerId = undefined;
         this.state = {
             message: '',
-            messageAnimationIndex: 0
         };
         this.onShutterAnimationEnd = this.onShutterAnimationEnd.bind(this);
-        this.onTimer = this.onTimer.bind(this);
-        this.restart();
     }
 
 
     componentDidMount() {
         let shutter = this.shutterRef.current;
-        shutter.addEventListener('animationend', this.onShutterAnimationEnd);
+        shutter.addEventListener('animationiteration', this.onShutterAnimationEnd);
     }
 
 
     componentWillUnmount() {
         let shutter = this.shutterRef.current;
-        shutter.removeEventListener('animationend', this.onShutterAnimationEnd);
+        shutter.removeEventListener('animationiteration', this.onShutterAnimationEnd);
     }
 
 
     onShutterAnimationEnd() {
-        if (++this.alertIndex >= this.currAlerts.length) {
-            this.restart();
-        }
-        else {
-            this.animateNext();
-        }
-    }
-
-
-    restart() {
-        clearTimeout(this.timerId);
-        this.timerId = undefined;
-        this.alertIndex = 0;
-        this.currAlerts = alertsStorage.getAlerts();
-        if (!this.currAlerts.length) {
-            this.timerId = setTimeout(this.onTimer, 1000);
-        }
-        else {
-            this.animateNext();
-        }
+        this.animateNext();
     }
 
 
     animateNext() {
-        let item = this.currAlerts[this.alertIndex];
-        let message = allAlerts.byId(item.id).text;
-        this.setState(oldState => {
-            let newState = Object.assign({}, oldState);
-            newState.message = message;
-            newState.messageAnimationIndex = oldState.messageAnimationIndex ? 0 : 1;
-            return newState;
+        if (++this.alertIndex >= this.currAlerts.length) {
+            this.alertIndex = 0;
+            this.currAlerts = alertsStorage.getAlerts();
+        }
+        if (this.currAlerts.length) {
+            let item = this.currAlerts[this.alertIndex];
+            var message = allAlerts.byId(item.id).text;
+        }
+        this.setState({
+            message: message ?? '',
         });
     }
 
 
-    onTimer() {
-        this.restart();
-    }
-
-
     render() {
-        let message = this.state.message;
-        let shutterAnimation = !message
-          ? 'none'
-          : 'tablo-shutter-move' + this.state.messageAnimationIndex;
         return  <div class="alerts-tablo">
-                    <div class="alerts-table-message">
-                        {message}
-                    </div>
                     <div class="alerts-tablo-shutter"
-                         style={{"animation-name": shutterAnimation}}
                          ref={this.shutterRef} />
+                    <div class="alerts-table-message">
+                        {this.state.message}
+                    </div>
                 </div>;
     }
 }
